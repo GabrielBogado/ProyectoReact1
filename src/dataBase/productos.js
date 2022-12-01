387e98581990fd2fa0b5285c58d710db498347da
Gabriel Bogado <bogado.gabriel141@gmail.com>
2022-12-01 20:29:27 -0300

    agregandoArrayProductos

---
+++ src/dataBase/productos.js
@@ -0,0 +1,115 @@
+const productos = 
+[
+    {
+        id:1,
+        nombre:"BGH SILENT AIR",
+        precio:72000,
+        stock:10,
+        tipo:"AIRE VENTANA",
+        imagen:"../img/aireVentana.jpg"
+    },
+    {
+        id:2,
+        nombre:"BGH TURBO AIR",
+        precio:102000,
+        stock:10,
+        tipo:"AIRE SPLIT",
+        imagen:"../img/bghSplit.jpg"
+    },
+    {
+        id:3,
+        nombre:"SURREY XCOLD",
+        precio:120000,
+        stock:10,
+        tipo:"AIRE VENTANA",
+        imagen:"../img/aireVentana.jpg"
+    },
+    {
+        id:4,
+        nombre:"NEX FREE AIR",
+        precio:89000,
+        stock:10,
+        tipo:"AIRE VENTANA",
+        imagen:"../img/aireVentana.jpg"
+    },
+    {
+        id:5,
+        nombre:"SANSEI INVERTER",
+        precio:132000,
+        stock:10,
+        tipo:"AIRE SPLIT",
+        imagen:"../img/sanseiSplit.jpg"
+    },
+    {
+        id:6,
+        nombre:"PHILCO 2881",
+        precio:96000,
+        stock:10,
+        tipo:"AIRE SPLIT",
+        imagen:"../img/sanseiSplit.jpg"
+    },
+    {
+        id:7,
+        nombre:"TCL ELITE",
+        precio:93000,
+        stock:10,
+        tipo:"AIRE SPLIT",
+        imagen:"../img/sanseiSplit.jpg"
+    },
+    {
+        id:8,
+        nombre:"LILIANA VP16P",
+        precio:12600,
+        stock:10,
+        tipo:"VENTILADOR PIE",
+        imagen:"../img/ventiladorPie.jpg"
+    },
+    {
+        id:9,
+        nombre:"INDELPLAS IV16",
+        precio:7200,
+        stock:10,
+        tipo:"VENTILADOR PIE",
+        imagen:"../img/ventiladorPie.jpg"
+    },
+    {
+        id:10,
+        nombre:"PEABODY PE-VTR42",
+        precio:31800,
+        stock:10,
+        tipo:"VENTILADOR TECHO",
+        imagen:"../img/ventiladoTecho.jpg"
+    },
+    {
+        id:11,
+        nombre:"KANJI KJH-FH1209",
+        precio:10900,
+        stock:10,
+        tipo:"VENTILADOR 3 EN 1",
+        imagen:"../img/ventilador3en1.jpg"
+    },
+    {
+        id:12,
+        nombre:"AXEL AX-VTPOTAR",
+        precio:25900,
+        stock:10,
+        tipo:"VENTILADOR TECHO",
+        imagen:"../img/ventiladoTecho.jpg"
+    },
+    {
+        id:13,
+        nombre:"LILIANA VW2016",
+        precio:17300,
+        stock:10,
+        tipo:"VENTILADOR DE PARED",
+        imagen:"../img/ventiladorPared.jpg"
+    },
+    {
+        id:14,
+        nombre:"BGH SILENT AIR TURBO",
+        precio:72000,
+        stock:10,
+        tipo:"AIRE SPLIT",
+        imagen:"../img/bghSplit.jpg"
+    }
+]
