c12c0f777f16ea458c0a7d716591e2870dfbce26
Gabriel Bogado <bogado.gabriel141@gmail.com>
2023-01-03 19:59:30 -0300

    agregandoFirebase

---
+++ src/services/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB8wRYKat1mhpjdk24uOmAOozcT1AuyuBU",
+  authDomain: "veranoff-a04a9.firebaseapp.com",
+  projectId: "veranoff-a04a9",
+  storageBucket: "veranoff-a04a9.appspot.com",
+  messagingSenderId: "1003175284094",
+  appId: "1:1003175284094:web:cea64941f2e563f2b1f72f"
+};
+
+const app = initializeApp(firebaseConfig);
